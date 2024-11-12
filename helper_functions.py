43280e91aa1e1c7ba648f1d366cabf5958d8afd0
emmenru <emmenru@gmail.com>
2024-11-12 15:12:20 +0100

    Clean up JSON imports & recode 7-step to 3-step scale

--- helper_functions.py
+++ helper_functions.py
@@ -4,33 +4,25 @@ import pandas as pd
 import subprocess
 
 # Formatting categorical data
-def load_ordinal_columns(script_path: str, json_file: str) -> dict:
+def load_recode_mappings(json_file: str) -> dict:
     '''
-    Runs a script to generate a JSON file with column category order, and loads the JSON file.
+    Loads recode mappings from a JSON file.
     
     Parameters:
-    - script_path (str): Path to the Python script generating the JSON file.
-    - json_file (str): Path to the JSON file to be loaded.
+    - json_file (str): Path to the JSON file containing recode mappings.
     
     Returns:
-    - dict: Loaded dictionary from the JSON file.
+    - dict: Loaded dictionary of recode mappings.
     
     Raises:
-    - subprocess.CalledProcessError: If the script fails to run.
     - FileNotFoundError: If the JSON file does not exist.
     '''
-    try:
-        subprocess.run(['python', script_path], capture_output=True, text=True, check=True)
-        print(f'{script_path} ran successfully.')
-    except subprocess.CalledProcessError as e:
-        print(f'Error running {script_path}: {e.stderr}')
-        raise
-
     try:
         with open(json_file, 'r') as file:
             return json.load(file)
     except FileNotFoundError:
-        raise FileNotFoundError(f'{json_file} was not created. Check {script_path} for issues.')
+        raise FileNotFoundError(f'{json_file} does not exist. Please check the file path.')
+
 
 def convert_to_categorical(col, ordinal_columns):
     '''
